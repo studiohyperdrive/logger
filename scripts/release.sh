044be6bda8bda5b0831dc0d9f8f82fa6921d0666
Niels Bril <niels.bril@hotmail.com>
2018-11-05 21:01:52 +0100

    Polish release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,5 @@
 ### PUBLISH
 
-printf "\e[94m 🚀\Releasing @studiohyperdrive/logger\e[39m\n\n"
+printf "\e[94m 🚀\tReleasing @studiohyperdrive/logger\e[39m\n\n"
 npm publish --access public
 printf "\e[92m 🎉\t@studiohyperdrive/logger has been successfully released\e[39m\n\n"
